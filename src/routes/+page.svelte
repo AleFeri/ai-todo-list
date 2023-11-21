bea1d27432e52a5be48bb3827ab7772694f315fb
AleFeri <50620354+AleFeri@users.noreply.github.com>
2023-11-21 11:40:45 +0100

    Added tailwind + init site first page creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,36 @@
 <script>
-    import Index from "../components/index.svelte";
+    import Index from "../components/main-page.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Index></Index>
+<style lang="postcss">
+  :root {
+    --background-color: #020203;
+    --text-color: #EDF2F7;
+    --accent-color: #5B8BB9;
+    --primary-color: #7BA2C6;
+    --secondary-color: #152432;
+  }
+
+  :global(html) {
+    color: var(--text-color);
+    background-color: var(--background-color);
+  }
+
+  .main-page {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .left {
+  }
+
+  .right {
+    background-color: var(--text-color);
+    color: var(--background-color);
+  }
+</style>
+
+<div class="main-page flex justify-center items-center p-2">
+  <div class="left flex-1 h-full p-2">ciao</div>
+  <div class="right flex-1 h-full rounded-lg p-16">bello</div>
+</div>
