e6162a8965b162ec51d4727734c4ab5203f09ab2
Riccardo De Luca <delucariccardo@hotmail.com>
2023-12-14 11:31:35 +0100

    reimplementing todo-tile

--- src/components/todo-tile.svelte
+++ src/components/todo-tile.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
-	import type { Todo } from "../module/types.ts";
-
+	import type { Todo } from "../module/types";
 	export let todo: Todo;
+
 	let inputElement: HTMLInputElement;
 	let edit = false;
-	let showMenu = false;
-
+	let showOptions = false;
+	
 	async function handleFocus() {
 		inputElement.selectionStart = inputElement.selectionEnd =
 			inputElement.value.length;
 	}
 
-	function setShowMenu(newValue = true) {
-		showMenu = newValue;
-	}
+	function test() {}
 </script>
 
 <li
 	class="w-full rounded-lg hover:bg-white"
-	on:mouseenter={() => setShowMenu()}
-	on:mouseleave={() => setShowMenu(false)}
+	on:mouseenter={() => (showOptions = true)}
+	on:mouseleave={() => (showOptions = false)}
 >
 	<div class="flex items-center gap-4 py-2">
 		<input
@@ -27,7 +25,23 @@
 			bind:checked={todo.checked}
 			class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2"
 		/>
+		{#if edit}
+			<input
+				type="text"
+				on:focusout={() => (edit = false)}
+				on:focus={() => handleFocus()}
+				bind:value={todo.title}
+				bind:this={inputElement}
+				class="flex-1 bg-transparent"
+			/>
+		{:else}
+			<div class="flex-1" on:click={() => (edit = true)}>
+				{todo.title}
+			</div>
+		{/if}
 
-		<div class="flex-1">{todo.title}</div>
+		{#if showOptions}
+			<button on:click={test}>DEL</button>
+		{/if}
 	</div>
 </li>
